b830cdfec7402398d70696acabd3cbdc66dea284
Michal Sakowicz <michalsakowicz99@gmail.com>
2020-05-04 15:51:27 +0200

    added exceptions

--- scraper.py
+++ scraper.py
@@ -1,37 +1,43 @@
 import imaplib
 import email
 from email.header import decode_header
-import webbrowser
 import os
 import re
 
 
 def log():
     print("Write down your login and password\nLogin:")
-    username = input()
+    login = input()
     print("Pasword:")
     password = input()
 
-    imap = imaplib.IMAP4_SSL("imap.wp.pl")
-    imap.login(username, password)
+    reg_exp = re.compile(r'@\w+.\w+')
+    domain = reg_exp.findall(login)
+    domain = str(domain).replace('@', "").replace('[', "").replace(']', "").replace("'", "").replace("'", "")
+
+    imap = imaplib.IMAP4_SSL("imap.{}".format(domain))
+    imap.login(login, password)
     return imap
 
 
 def reading_emails():
     mail_interior = log()
-    il_char = ['<', '>', '/', '%', '&', '#', '?', ':']
     status, messages = mail_interior.select('INBOX')
-    how_many_less = 850
+    how_many = 20
+    z = 0
     mail_nb = int(messages[0])
 
-    for i in range(mail_nb, mail_nb-how_many_less, -1):
+    for i in range(mail_nb, mail_nb-how_many, -1):
         res, msg = mail_interior.fetch(str(i), "(RFC822)")
         for response in msg:
             if isinstance(response, tuple):
                 msg = email.message_from_bytes(response[1])
                 subject = decode_header(msg["Subject"])[0][0]
                 if isinstance(subject, bytes):
-                    subject = subject.decode()
+                    try:
+                        subject = subject.decode()
+                    except UnicodeDecodeError:
+                        pass
                 from_ = msg.get("From")
                 print("Subject: ", subject)
                 print("From: ", from_)
@@ -48,31 +54,55 @@ def reading_emails():
                         elif "attachment" in content_disposition:
                             filename = part.get_filename()
                             if filename:
-                                if not os.path.isdir(subject):              #MAKING FOLDERS
-                                    reg_expr = re.compile(r'\w+')
-                                    subject = str(reg_expr.findall(subject))
-                                    subject = subject.replace(",", "").replace("'", "").replace('[', "").replace(']', "")
-                                    os.mkdir(subject)
-                                filepath = os.path.join(subject, filename)
-                                open(filepath, "wb").write(part.get_payload(decode=True))
-                    else:
-                        content_type = msg.get_content_type()   #string type
-                        body = str(msg.get_payload(decode=True))     #here is the problem
+                                try:
+                                    if not os.path.isdir(subject):
+                                        reg_expr = re.compile(r'\w+')
+                                        subject = str(reg_expr.findall(subject))
+                                        subject = subject.replace(",", "").replace("'", "").replace('[', "").replace(']', "")
+                                        try:
+                                            os.mkdir(subject)
+                                        except FileExistsError:
+                                            subject = subject + ' ({})'.format(str(z))
+                                            z += 1
+                                            os.mkdir(subject)
+                                    filepath = os.path.join(subject, filename)
+                                except UnicodeDecodeError:
+                                    pass
+                                try:
+                                    open(filepath, "wb").write(part.get_payload(decode=True))
+                                except OSError:
+                                    pass
+                else:
+                    content_type = msg.get_content_type()
+                    try:
+                        body = msg.get_payload(decode=True).decode()     #here is the problem
                         if content_type == "text/plain":
                             print(body)
-                    if content_type == "text/html":
+                    except UnicodeDecodeError:
+                        pass
+                if content_type == "text/html":
+                    try:
                         if not os.path.isdir(subject):
                             reg_expr = re.compile(r'\w+')
                             subject = str(reg_expr.findall(subject))
                             subject = subject.replace(",", "").replace("'", "").replace('[', "").replace(']', "")
-                            os.mkdir(subject)
+                            try:
+                                os.mkdir(subject)
+                            except FileExistsError:
+                                subject = subject + ' ({})'.format(str(z))
+                                z += 1
+                                os.mkdir(subject)
                         filename = f"{subject[:50]}.html"
                         filepath = os.path.join(subject, filename)
-                        open(filepath, "w").write(body)
-                        webbrowser.open(filepath)
-                    print("="*100)
+                        try:
+                            open(filepath, "w").write(body)
+                        except (UnicodeEncodeError, FileNotFoundError):
+                            pass
+                    except UnicodeDecodeError:
+                        pass
     mail_interior.close()
     mail_interior.logout()
+
 
 if __name__ == "__main__":
     reading_emails()
